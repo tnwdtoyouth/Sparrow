3af618d29645385fbddb56f9cff6d13602e08001
Yevhen <evventol@gmail.com>
2021-05-22 22:56:18 +0300

    one fix

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 const static = require('node-static');
-
+const fs = require("fs");
 const file = new static.Server('./public');
+var express = require('express');
+var app = express();
 
-const PORT = 4002;
-require('http')
-  .createServer(function(request, response) {
-    request
-      .addListener('end', function() {
-        file.serve(request, response);
-      })
-      .resume();
-  })
-  .listen(PORT, function() {
-    console.log(`server is on ${PORT}`);
-  });
+app.use('/static',express.static('public'));
+app.set("view engine", "ejs");
+app.get('/', function(req, res) {
+  res.render('index.ejs');
+});
+app.get('/recognition', function(req, res) {
+  res.render('recognition.ejs');
+});
+app.get('/synthesis', function(req, res) {
+  res.render('synthesis.ejs');
+});
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!');
+});
+
